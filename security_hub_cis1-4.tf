b2191ad3f48f0b93592724a1d91748a471bdf6c4
Justin C <justinc71@gmail.com>
2023-04-06 12:06:15 -0700

    security hub tf created

---
+++ security_hub_cis1-4.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_securityhub_account" "this" {}
+
+resource "aws_securityhub_standards_subscription" "cis" {
+  depends_on = [aws_securityhub_account.this]
+
+  standards_arn = "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0"
+}
