4598ff1961afef6d7e5035338da07c0ee1d91773
Justin <justinc71@gmail.com>
2023-04-14 08:36:06 -0700

    Update security_hub_cis1-4.tf
    Updated to 1.4

--- security_hub_cis1-4.tf
+++ security_hub_cis1-4.tf
@@ -7,5 +7,5 @@ resource "aws_securityhub_account" "this" {}
 resource "aws_securityhub_standards_subscription" "cis" {
   depends_on = [aws_securityhub_account.this]
 
-  standards_arn = "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0"
+  standards_arn = "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.4.0"
 }
